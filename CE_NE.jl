5d7c3ce28a4fcc9f474eb83076b99a99520e383e
rbassett3 <thomas.e.hallward@gmail.com>
2015-08-07 11:09:34 -0700

    After a long pause, I'm back. Added references and spacing in the code for readability.

--- CE_NE.jl
+++ CE_NE.jl
@@ -1,3 +1,6 @@
+#This is a script that compute a correlated equilibria and then uses
+#progressive hedging to transform it to a Nash Equilibria
+
 using PyPlot
 using Convex
 using Debug
@@ -23,7 +26,8 @@ U[2]=[4 5; 1 0]
 
 
 #Getting into the optimization
-P=Variable(n,m, Positive())
+P=Variable(n,m, Positive()) #A Variable that represents the mixed
+#strategies of player 1 and player 2
 
 problem = maximize(sum(P.*U[1]+P.*U[2]))
 problem.constraints += [sum(P)==1]
@@ -58,37 +62,44 @@ g=zeros(100,1) #error stuff
 
 for k=1:100
 
-#Find the Marginals
-M=Array(Array{Float64,2}, N)
-for i=1:N
-	M[i]=sum(X,N+1-i)
-end
+    #Find the Marginals
+    M=Array(Array{Float64,2}, N)
+    for i=1:N
+        M[i]=sum(X,N+1-i)
+    end
 
-Xhat=prod(M)
-W=W+r*(X-Xhat)
-D[k]=norm(X-Xhat)
-g[k]=k
+    Xhat=prod(M)
+    W=W+r*(X-Xhat)
+    D[k]=norm(X-Xhat)
+    g[k]=k
 
-if k==1
-	W=0
-end
+    if k==1
+	    W=0
+    end
 
-problem = minimize(-1*sum(P.*U[1]+P*U[2])+sum(P.*W)+(1/2)*r*norm(P-Xhat)^2)
-problem.constraints += [sum(P)==1]
+    problem = minimize(-1*sum(P.*U[1]+P.*U[2])+sum(P.*W)+(1/2)*r*norm(P-Xhat)^2)
+    problem.constraints += [sum(P)==1]
 
-#Adding the constraints for player 1
-for i = 1:n, q =1:n
-		problem.constraints += [sum((U[1][i,:]-U[1][q,:]).*P[i,:]) >=0]
-end
+    #Adding the constraints for player 1
+    for i = 1:n, q =1:n
+		problem.constraints +=[sum((U[1][i,:]-U[1][q,:]).*P[i,:])>=0] 
+        #This constraint and the one that follows needs a citation
+        #Maybe it's self explanatory. The expected value of playing 
+        #q is less than the expected value of playing i.
+        #Perhaps I have included it in the paper. Either way Remark
+        #2.1 Here:
+        #https://www.ceremade.dauphine.fr/~viossat/pdfs/Geom-Cor-ZSG-Swopec.pdf 
+        #contains what I need
+     end
 
-#Now for player 2
-for j = 1:m, q =1:m
+    #Now for player 2
+    for j = 1:m, q =1:m
 		problem.constraints += [sum((U[2][:,j]-U[2][:,q]).*P[:,j]) >=0]
-end
+    end
 
-solve!(problem, SCSSolver(max_iters=100000))
+    solve!(problem, SCSSolver(max_iters=100000))
 
-X=P.value
+    X=P.value
 
 end
 	
@@ -97,7 +108,7 @@ end #for debug
 NE=P.value
 
 if norm(NE-CE)>.01
-println("Max utility NE is not Max Utility CE")
+    println("Max utility NE is not Max Utility CE")
 end
 
 clf()
